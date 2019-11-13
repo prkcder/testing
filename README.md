11738cefc56024e172b02b2a6fd573464f43e176
John Park <jhnprk12@gmail.com>
2019-11-13 15:19:23 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# Testing
+
+This repo is used to play around with different testing methods.
+Clone down the repo and play around with jest, cypress, selenium etc.
+Mess around with unit testing or end-2-end testing. Overall
+have fun with it.
+
+
+## Steps to clone and Start on Your local machine
+
+Copy the url then type the following below in your terminal
+
+```
+git clone [url]
+```
+
+Then type `git remote remove origin` so the repo on your machine is no longer connected to the repo on github. 
+The following steps below will have you install all the dependencies and 
+start the server for a sample page. The last step is the command for testing.
+
+```
+npm install
+```
+```
+npm start
+```
+```
+npm run test
+```
---
+++ tests/sum.test.js
@@ -0,0 +1,55 @@
+const sum = require('../public/sum');
+
+// test('adds 1 + 2 to equal 3', () => {
+//   expect(sum(1, 2)).toBe(3);
+// });
+
+// test('two plus two is four', () => {
+//     expect(2 + 2).toBe(4);
+//   });
+
+// test('test', () => {
+//     expect(sum(4, 5)).toBe(9);
+// });
+
+//   test('object assignment', () => {
+//     const data = {one: 1};
+//     data['two'] = 2;
+//     expect(data).toEqual({one: 1, two: 2});
+//   });
+
+//   test('adding positive numbers is not zero', () => {
+//     for (let a = 1; a < 10; a++) {
+//       for (let b = 1; b < 10; b++) {
+//         expect(a + b).not.toBe(0);
+//       }
+//     }
+//   });
+
+// test('two plus two', () => {
+//     const value = sum(2, 2);
+//     expect(value).toBeGreaterThan(3);
+//     expect(value).toBeGreaterThanOrEqual(3.5);
+//     expect(value).toBeLessThan(5);
+//     expect(value).toBeLessThanOrEqual(4.5);
+  
+//     // toBe and toEqual are equivalent for numbers
+//     expect(value).toBe(4);
+//     expect(value).toEqual(4);
+//   });
+
+test('adding floating point numbers', () => {
+    const value = 0.1 + 0.2;
+    //expect(value).toBe(0.3);           This won't work because of rounding error
+    expect(value).toBeCloseTo(0.3); // This works.
+  });
+
+
+  test('there is no I in team', () => {
+    expect('team').not.toMatch(/I/);
+  });
+  
+  test('but there is a "stop" in Christoph', () => {
+    expect('Christoph').toMatch(/stop/);
+  });
+
